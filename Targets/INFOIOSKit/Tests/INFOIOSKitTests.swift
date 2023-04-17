8e5dd716e294866d128245d6e69a69ed380e5458
junha <goodjunha@gmail.com>
2023-04-17 16:00:19 +0900

    Build :: tuist

---
+++ Targets/INFOIOSKit/Tests/INFOIOSKitTests.swift
@@ -0,0 +1,8 @@
+import Foundation
+import XCTest
+
+final class INFOIOSKitTests: XCTestCase {
+    func test_example() {
+        XCTAssertEqual("INFOIOSKit", "INFOIOSKit")
+    }
+}
---
+++ Targets/INFOIOSUI/Tests/INFOIOSUITests.swift
@@ -0,0 +1,8 @@
+import Foundation
+import XCTest
+
+final class INFOIOSUITests: XCTestCase {
+    func test_example() {
+        XCTAssertEqual("INFOIOSUI", "INFOIOSUI")
+    }
+}
