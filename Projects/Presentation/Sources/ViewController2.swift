23e5aa5a909b124149d97a28c95b328e5328b142
junha <goodjunha@gmail.com>
2023-04-17 23:12:22 +0900

    Test:: Flow ViewController

--- Projects/Presentation/Sources/ViewController2.swift
+++ Projects/Presentation/Sources/ViewController2.swift
@@ -7,14 +7,27 @@
 //
 
 import UIKit
+import SnapKit
+import Then
 
 public class ViewController2: UIViewController {
+   
+    var button = UIButton().then {
+        $0.backgroundColor = .green
+    }
     
     public var factory: ModuleFactoryInterface!
     
     public override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .red
+        view.backgroundColor = .blue
+        
+        view.addSubview(button)
+        
+        button.snp.makeConstraints {
+            $0.height.width.equalTo(100)
+            $0.centerX.centerY.equalToSuperview()
+        }
     }
 }
